85ab212166c9aeb9eed176eb0dc831d4117f5c21
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-06-03 08:29:43 +0300

    Added stripe checkout seesion to button

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -6,14 +6,21 @@ import { BASE_PRICE, PRODUCT_PRICES } from '@/config/products'
 import { cn, formatPrice } from '@/lib/utils'
 import { COLORS, FINISHES, MODELS } from '@/validators/options-validators'
 import { Configuration } from '@prisma/client'
+import { useMutation } from '@tanstack/react-query'
 import { ArrowRight, Check } from 'lucide-react'
 import { useEffect, useState } from 'react'
 import Confetti from 'react-dom-confetti'
+import { createCheckoutSession } from './actions'
+import { useRouter } from 'next/navigation'
+import { useToast } from '@/components/ui/use-toast'
 
 const DesignPreview = ({configuration}: {configuration: Configuration}) => {
 
   const [showConfetti, setShowConfetti] = useState(false)
   useEffect(() => setShowConfetti(true), [])
+
+  const router = useRouter()
+  const {toast} = useToast()
 
   const { color, model, finish, material } = configuration
 
@@ -27,6 +34,22 @@ const DesignPreview = ({configuration}: {configuration: Configuration}) => {
 
   if(finish === 'textured')
     totalPrice += PRODUCT_PRICES.finish.textured
+
+  const {mutate: createPaymentSession} = useMutation({
+    mutationKey: ["get-checkout-session"],
+    mutationFn:  createCheckoutSession,
+    onSuccess: ({url}) => {
+        if(url) router.push(url)
+            else throw new Error("Unable to retrieve payment url")
+    },
+    onError:() => {
+        toast({
+            title: "Something went wrong",
+            description: "There was an error on our end. Please try again.",
+            variant: 'destructive'
+        })
+    }
+  })
 
 
   return (
@@ -120,6 +143,7 @@ const DesignPreview = ({configuration}: {configuration: Configuration}) => {
                     <div className='mt-8 flex justify-end pb-12'>
                         <Button
                             className='px-4 sm:px-6 lg:px-8'
+                            onClick={() => createPaymentSession({configId: configuration.id})}                            
                         >
                             Check out <ArrowRight className='h-4 w-4 ml-1.5 inline'/>
                         </Button>
