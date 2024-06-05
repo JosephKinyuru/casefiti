bd4279083c572ab7c82682eb972394c7509324ef
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-06-05 13:32:56 +0300

    Added user button and dashboard for user/admin

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,12 @@ import MaxWidthWrapper from "./MaxWidthWrapper"
 import { buttonVariants } from "./ui/button"
 import { ArrowRight } from "lucide-react"
 import { currentUser } from "@clerk/nextjs/server"
+import { UserButton } from "@clerk/nextjs"
 
 const Navbar = async () => {
 
   const user = await currentUser()
-  const isAdmin = user?.primaryEmailAddress === process.env.ADMIN_EMAIL
+  const isAdmin = user?.primaryEmailAddress?.emailAddress === process.env.ADMIN_EMAIL
 
   return (
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
@@ -19,14 +20,7 @@ const Navbar = async () => {
                 <div className="h-full flex items-center space-x-4">
                     {user ? (
                     <>
-                        <Link 
-                            href={"/api/auth/logout"} 
-                            className={buttonVariants({
-                                size: 'sm', 
-                                variant: 'ghost',
-                        })}>
-                            Sign out
-                        </Link>
+                        <UserButton showName/>
                         {isAdmin ? <Link 
                             href={"/api/auth/logout"} 
                             className={buttonVariants({
