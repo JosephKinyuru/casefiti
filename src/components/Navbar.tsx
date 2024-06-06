65939b193ea53b7cf1e327c0df816235419c1eeb
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-06-06 17:02:27 +0300

    Changes to navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = async () => {
                     <>
                         <UserButton showName/>
                         {isAdmin ? <Link 
-                            href={"/api/auth/logout"} 
+                            href={"/dashboard"} 
                             className={buttonVariants({
                                 size: 'sm', 
                                 variant: 'ghost',
