3adaafa6043d644d5b99260c5b380a9d62b5b6b7
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-06-06 17:03:16 +0300

    Metadata constructor added

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next"
 import { twMerge } from "tailwind-merge"
+import { string } from "zod"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -13,4 +15,34 @@ export const formatPrice = (price:number) => {
 
 
   return formatter.format(price)
+}
+
+export function constructMetadata({
+  title = "Casecobra custom high-quality phone cases",
+  description = "Create custom high1-quality phone cases in seconds",
+  image = '/thumbnail.png',
+  icons = '/favicon.ico'
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+} = {}) : Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title, 
+      description,
+      images: [{url: image}]
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@josephk1nyuru",
+    },
+    icons,
+  }
 }
