bd1d60a96a99db296c0ee4bcfdb3ba66379335d5
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-08-10 07:32:24 +0300

    Changed resend email source

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -72,7 +72,7 @@ export async function POST(req: Request) {
       })
 
       await resend.emails.send({
-        from: 'CaseFiti <josephkinyuru22@gmail.com>',
+        from: 'CaseFiti <send@josephkinyuru.com>',
         to: [event.data.object.customer_details.email],
         subject: 'Thanks for your order!',
         react: OrderReceivedEmail({
