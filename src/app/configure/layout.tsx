908e8cab97063c7794ec31cfd50bb7a0cb819884
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-08-13 18:25:15 +0300

    Fixed mobile navbar issue on configure page

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react"
 
 const Layout = ({children}: {children: ReactNode}) => {
   return (
-    <MaxWidthWrapper className="flex-1 flex flex-col">
+    <MaxWidthWrapper className="flex-1 flex flex-col mt-14 sm:mt-0">
         <Steps/>
         {children}
     </MaxWidthWrapper>
